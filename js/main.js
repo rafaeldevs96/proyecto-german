d349d41de72c23a33cbfb93aca807b69f1c3c325
Rafael <rafael1996gc@gmail.com>
2025-12-05 18:12:20 -0400

    fase 1

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+
+// Cerrar menú al hacer click en un enlace (Mejora UX)
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}));
